20d277aaeb3c9729a0a7d5b44455f36d633d19be
madsrohdin <73933066+madsrohdin@users.noreply.github.com>
2021-02-03 00:36:48 +0100

    Add EkstraOpgave1

---
+++ EkstraOpgave1/EkstraOpgave1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EkstraOpgave1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int retirementYear;
+            int Years;
+            int Retirement;
+            Console.WriteLine("What is your current age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("At what age do you wish to retire?");
+            retirementYear = Convert.ToInt32(Console.ReadLine());
+            Years = retirementYear - age;
+            string år = "2021";
+            DateTime dt = new DateTime(int.Parse(år), 1, 1);
+            Retirement = dt.Year + Years;
+            if (dt.Year > Retirement)
+            {
+                Console.WriteLine("Congratulations, you can already retire");
+            }
+            else
+            {
+                Console.WriteLine("It's " + dt.ToString("yyyy") + ", so you can retire in {0}", Retirement);
+            }
+        }
+    }
+}
