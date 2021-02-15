69715df0caa2c6397e5878859bc7f5ab72d3ee41
madsrohdin <73933066+madsrohdin@users.noreply.github.com>
2021-02-15 23:12:06 +0100

    Added EkstraOpgave3

---
+++ EkstraOpgave3/EkstraOpgave3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EkstraOpgave3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0, r, t, n, P; 
+            Console.WriteLine("What is the principal amount?");
+            P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the annual rate of interest?");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the number of years the ammount is invested?");
+            t = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the number of times the interest is compounded per year?");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            double krop = 1 + (r / 100 / n);
+            double eksponent = n * t;
+            A = P * Math.Pow(krop, eksponent);
+
+            Console.WriteLine("${0} invested at {1}% for {2} years compunded {3} times per year is ${4}", P, r, t, n, A);
+        }
+    }
+}
