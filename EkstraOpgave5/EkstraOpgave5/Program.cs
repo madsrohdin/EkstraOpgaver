9a9ad78dd97c57936bb8a9a25ed3a6b15b2d9a6e
madsrohdin <73933066+madsrohdin@users.noreply.github.com>
2021-02-17 00:28:43 +0100

    Add EkstraOpgave5

---
+++ EkstraOpgave5/EkstraOpgave5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EkstraOpgave5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+
+            Console.WriteLine("Please enter a password:");
+            password = Console.ReadLine();
+
+            //Very weak
+            if (password.All(char.IsDigit) && password.Length < 8)
+            {
+                Console.WriteLine("The password '{0}' is a very weak password.", password);
+            }
+
+            //Weak
+            else if (password.All(char.IsLetter) && password.Length < 8)
+            {
+                Console.WriteLine("The password '{0}' is a weak password.", password);
+            }
+
+            //Very strong
+            else if (password.Any(char.IsDigit) && password.Any(char.IsLetter) && password.Any(char.IsPunctuation) && password.Length >= 8)
+            {
+                Console.WriteLine("The password '{0}' is a very strong password", password);
+            }
+
+            //Strong
+            else if (password.Any(char.IsLetter) && password.Any(char.IsDigit) && password.Length >= 8)
+            {
+                Console.WriteLine("The password '{0}' is a strong password.", password);
+            }
+        }
+    }
+}
