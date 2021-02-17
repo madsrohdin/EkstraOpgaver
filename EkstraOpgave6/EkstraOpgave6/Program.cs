5eb6d971fd6d2fae2434f95fc7364074ad5014eb
madsrohdin <73933066+madsrohdin@users.noreply.github.com>
2021-02-17 22:00:55 +0100

    Add EkstraOpgave6

---
+++ EkstraOpgave6/EkstraOpgave6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace EkstraOpgave6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Terminate;
+            ArrayList employees = new ArrayList();
+            employees.Add("John Smith");
+            employees.Add("Jackie Jackson");
+            employees.Add("Chris Jones");
+            employees.Add("Amanda Cullen");
+            employees.Add("Jeremy Goodwin");
+
+            Console.WriteLine("There are {0} employees:", employees.Count);
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
+            Console.WriteLine("\nEnter an employee to remove:");
+            Terminate = Console.ReadLine();
+
+            if (employees.Contains(Terminate))
+            {
+                employees.Remove(Terminate);
+                Console.WriteLine("\nThere are {0} employees:", employees.Count);
+                foreach (var item in employees)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nEmployee not found.");
+            }
+        }
+    }
+}
