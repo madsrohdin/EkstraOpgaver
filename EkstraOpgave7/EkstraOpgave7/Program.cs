4ed8032981c691e358184a729e029532245c1d90
madsrohdin <73933066+madsrohdin@users.noreply.github.com>
2021-02-18 22:16:51 +0100

    Add EkstraOpgave7

---
+++ EkstraOpgave7/EkstraOpgave7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EkstraOpgave7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter a list of numbers, separated by spaces:");
+            input = Console.ReadLine();
+            var tal = input.Split(' ');
+            Console.WriteLine("The even numbers are:");
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                int numbers = Convert.ToInt32(tal[i]);
+                if (numbers % 2 == 0)
+                {
+                    Console.Write(numbers + " ");
+                }
+            }
+        }
+    }
+}
